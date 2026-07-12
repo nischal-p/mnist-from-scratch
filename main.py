aa2b152412bd191ebacfbd02e837749966aaa518
Nischal Poudel <nischalp@seas.upenn.edu>
2026-07-12 19:50:14 -0400

    adding notebook

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mnist-from-scratch!")
+from datasets import load_dataset
+import numpy as np
 
+dataset = load_dataset("ylecun/mnist")
 
-if __name__ == "__main__":
-    main()
+example = dataset["train"][0]
+
+image = example["image"]
+label = example["label"]
+
+pixels = np.array(image)
+
+print(pixels[0][0])
+print(pixels[14][14])
